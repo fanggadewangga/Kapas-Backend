9dbba2ddb520777c291da26b0068f1dc63ffd570
fanggadewangga <fanggadewangga@gmail.com>
2022-10-19 06:54:38 +0700

    change: some changes on Routing.kt

--- src/main/kotlin/com/kapas/plugins/Routing.kt
+++ src/main/kotlin/com/kapas/plugins/Routing.kt
@@ -4,6 +4,7 @@ import com.kapas.route.job.JobRoute
 import com.kapas.route.user.UserRoute
 import io.ktor.application.*
 import io.ktor.locations.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import org.koin.ktor.ext.inject
 
@@ -13,6 +14,14 @@ fun Application.configureRouting() {
     val jobRoute by inject<JobRoute>()
 
     routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        get("/leaderboards") {
+            call.respondText("Hello from leaderboard")
+        }
+
         userRoute.initUserRoute(this)
         jobRoute.initJobRoute(this)
     }
